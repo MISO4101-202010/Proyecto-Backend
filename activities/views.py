4c3a7021c89b5d8b33ba3126324e3ba424a8e3db
jfmatheusg <jfmatheusg@gmail.com>
2020-05-30 16:08:05 -0500

    pruebas con ambiente local postgres, ajustes scripts, ajustes pruebas postgres

--- activities/views.py
+++ activities/views.py
@@ -795,7 +795,7 @@ class GetReporteCalificaciones(ListModelMixin, GenericAPIView):
             notaTotal = sumCalificaciones / len(marcas)
             reporteCalificaciones = {"respuestasCorrectas": respuestasCorrectas,
                                      "respuestasIncorrectas": respuestasIncorrectas,
-                                     "calificacionTotal": round(notaTotal,2),
+                                     "calificacionTotal": str(round(notaTotal,2)),
                                      "calificaciones": listaCalifaciones}
             cursor.close()
             #Se retorna la respuesta
